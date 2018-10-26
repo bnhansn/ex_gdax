1ea6a3ac4fbef7911e18250b38cbbc798eb427b7
Ben Hansen <bnhansn@gmail.com>
2018-10-26 12:30:03 -0700

    update ex_doc version

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule ExGdax.Mixfile do
       {:poison, "~> 3.1"},
       {:mock, "~> 0.2.1", only: :test},
       {:credo, "~> 0.8.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.16.2", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:mix_test_watch, "~> 0.5", only: :dev, runtime: false}
     ]
   end
